d25e9c392a013e941a8c1d3651b29d1ce3a1c36b
Chazzz287 <vaniivanov55@gmail.com>
2025-05-13 15:10:12 +0400

    Зависимости +

---
+++ ThreeLayer/Task.ThreeLayer/Task.ThreeLayer.BLL/IPersonLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using Task.ThreeLayer.Entities;
+
+namespace Task.ThreeLayer.BLL
+{
+    public interface IPersonLogic
+    {
+        void AddPerson(string name);
+        void DeletePerson(string name);
+        void DeletePerson(int index);
+        IEnumerable GetAllPerson();
+        Person GetPerson(int index);
+    }
+}
