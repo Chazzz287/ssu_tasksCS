68856acf0b6d583cc49a84a35792cc4a2e6e0dee
Chazzz287 <vaniivanov55@gmail.com>
2025-05-14 00:20:31 +0400

    Rewriting everything from framework to core

---
+++ Task.ThreeLayer/Task.ThreeLayer.BLL/IPersonLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.ThreeLayer.Entities;
+
+namespace Task.ThreeLayer.BLL
+{
+    internal interface IPersonLogic
+    {
+        void AddPerson(string name);
+        void DeletePerson(string name);
+        void DeletePerson(int index);
+        IEnumerable GetAllPersons();
+        Person GetPerson(int index);
+    }
+}
---
+++ Task.ThreeLayer/Task.ThreeLayer.DAL/IBasePersons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.ThreeLayer.Entities;
+
+namespace Task.ThreeLayer.DAL
+{
+    internal interface IBasePersons
+    {
+
+        void AddPerson(Person person);
+        void DeletePerson(string name);
+        void DeletePerson(int index);
+        IEnumerable GetAllPersons();
+        Person GetPerson(int index);
+    }
+}
